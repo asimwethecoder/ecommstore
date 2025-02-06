1a1cc76119a40ff9f6174009f6ab2f713e05dab3
asimwethecoder <143520728+asimwethecoder@users.noreply.github.com>
2025-02-06 17:53:07 +0200

    modified file

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -52,7 +52,7 @@ export const updateProduct = async (req, res) => {
 };
 
 export const deleteProduct = async (req, res) => {
-  const { id } = req.params.id;
+  const { id } = req.params;
 
   try {
     await Product.findByIdAndDelete(id);
